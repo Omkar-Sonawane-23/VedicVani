5562b206feaef986db29b0054bcba9ceee3cd9b9
Harsh Mahadev Duche <ducheharsh@gmail.com>
2024-05-11 18:39:05 +0530

    heroku hosting

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const { bgChapter, bgSlok } = require('./db/db');
 const app = express();
-const port = 3001;
+const port = process.env.PORT || 3001;
 
 app.use(express.static('public'));
 

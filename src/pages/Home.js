c0cdabc68e19800d21338c8dca3d7f2ab5bff115
Harsh Mahadev Duche <ducheharsh@gmail.com>
2024-05-13 09:20:59 +0530

    "ignoring node modules"

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,6 @@ function Home() {
     // Use useEffect to fetch data when the component mounts
     async function fetchData() {
       try {
-
         const req = await fetch(`https://secure-island-88805-6239737da534.herokuapp.com/api/chapter/${selectedchap}`);
         const data = await req.json();
         const i = data.chapter.verses_count;

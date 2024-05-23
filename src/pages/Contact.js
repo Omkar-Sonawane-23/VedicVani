0da23ba33c0294048f669516cf60c84e692eed8f
Kushal1605 <kushalgupta1605@gmail.com>
2024-05-23 13:57:38 +0530

    Fix a bug in Contact Page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -12,7 +12,7 @@ const Contact = () => {
         <div id="Form_container">
         <div className="relative mb-8 flex justify-center items-center">
         <img data-aos="fade-right" style={{top:'1.60rem',zIndex:1}} className="backdrop-opacity-1 absolute w-[28rem] max-w-full  transform -translate-x-1/2" src={logounder} alt="logo" />
-        <p style={{fontSize:'5rem',color:'blue',position:'relative',zIndex:2}} className="font-[Alegreya] text-center">VedicVani</p>
+        <p style={{fontSize:'5rem',color:'blue',position:'relative',zIndex:1}} className="font-[Alegreya] text-center">VedicVani</p>
         </div>
         <p>Welcome to Vedic Vani! We're dedicated to sharing the wisdom and teachings of the Vedic tradition with the world. If you have any questions about our project, want to collaborate, or simply want to say hello, we'd love to hear from you. Whether you're seeking guidance, have feedback to share, or want to explore partnership opportunities, our team is here to assist you. Feel free to reach out to us through the contact details provided below. Your thoughts and inquiries are valuable to us, and we endeavor to respond promptly to all messages. Thank you for your interest in Vedic Vani - we look forward to connecting with you!</p>
         </div>
